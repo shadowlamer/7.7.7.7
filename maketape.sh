362364c64d605f26152219565fbf62b9a733395f
Vadim Cherenev <sl@anhot.ru>
2022-12-24 02:08:19 +0500

    Fix tape composer.

--- maketape.sh
+++ maketape.sh
@@ -7,7 +7,7 @@ CODE="${PWD}/bin/main.bin"
 INTRO="${PWD}/assets/intro.scr"
 SKIP=536
 
-read -r END REST < <(hexdump -C ${ROM} | tail -n 3 | head -n 1)
+read -r END REST < <(hexdump -C ${ROM} | tail -n 2 | head -n 1)
 dd if=${ROM} of=${CODE} bs=1 skip=${SKIP} count=$((0x${END}-${SKIP}))
 
 zmakebas -i 1 -a 10 -l -o ${TAPE} -n ${PROJECT} loader.bas
